1ccc6d8a8e88f1fb69972c8692218b8ff35468e0
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-10-18 20:00:21 -0300

    Adding alias to less showing colors

--- .bash/aliases.bash
+++ .bash/aliases.bash
@@ -55,3 +55,6 @@ alias ports='netstat -tulanp'
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+# less displaying colors
+export LESS="-R"

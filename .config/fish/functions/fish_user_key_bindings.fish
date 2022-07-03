855513ec2925ba8a37e8f404ba597ded1f2f276d
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2022-07-03 12:13:32 -0300

    Update fzf integration with fish

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,30 +1,3 @@
 function fish_user_key_bindings
     fzf_key_bindings
-    ### fzf ###
-    if test "$FZF_LEGACY_KEYBINDINGS" -eq 1
-        bind \ct '__fzf_find_file'
-        bind \cr '__fzf_reverse_isearch'
-        bind \ec '__fzf_cd'
-        bind \eC '__fzf_cd_with_hidden'
-        if bind -M insert >/dev/null ^/dev/null
-            bind -M insert \ct '__fzf_find_file'
-            bind -M insert \cr '__fzf_reverse_isearch'
-            bind -M insert \ec '__fzf_cd'
-            bind -M insert \eC '__fzf_cd --hidden'
-        end
-    else
-        bind \cf '__fzf_find_file'
-        bind \cr '__fzf_reverse_isearch'
-        bind \eo '__fzf_cd'
-        bind \eO '__fzf_cd --hidden'
-        if bind -M insert >/dev/null ^/dev/null
-            bind -M insert \cf '__fzf_find_file'
-            bind -M insert \cr '__fzf_reverse_isearch'
-            bind -M insert \eo '__fzf_cd'
-            bind -M insert \eO '__fzf_cd --hidden'
-        end
-    end
-    set -q FZF_COMPLETE
-    and bind \t '__fzf_complete'
-    ### fzf ###
 end
